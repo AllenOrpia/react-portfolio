7f1dc3d3ad67f14980d6c1ddb6aa92d42ceae7ff
Allen Orpia <allenorpia17@gmail.com>
2023-08-28 21:27:29 -0700

    fixing link again

--- src/Links.jsx
+++ src/Links.jsx
@@ -2,7 +2,7 @@
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import PictureAsPdfIcon from '@mui/icons-material/PictureAsPdf';
-import resume from "/Resume.pdf"
+
 
 export default function Links() {
     return (
@@ -15,7 +15,7 @@ export default function Links() {
                     <a href="https://github.com/AllenOrpia" className='flex justify-between items-center w-full text-white'>GitHub <GitHubIcon fontSize='large' /></a>
                 </li>
                 <li className='flex justify-between items-center w-40 h-14 px-4 bg-gradient-to-r from-cyan-500 via-cyan-600 to-blue-600 ml-[-100px] hover:ml-[-10px] hover:rounded-md duration-300 rounded-br-md'>
-                    <a href={resume} download='resume' className='flex justify-between items-center w-full text-white'>Resume <PictureAsPdfIcon fontSize='large' /></a>
+                    <a href="Resume.pdf" download='resume' className='flex justify-between items-center w-full text-white'>Resume <PictureAsPdfIcon fontSize='large' /></a>
                 </li>
             </ul>
         </div>
